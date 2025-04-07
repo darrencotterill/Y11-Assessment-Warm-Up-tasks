98e40eaf91f11187f3420b6eaee2dcda444ded91
darrencotterill <darren.cotterill2@det.nsw.edu.au>
2025-04-07 04:48:44 +0000

    add some sample practice ideas

---
+++ main.py
@@ -0,0 +1,16 @@
+
+# Input a number from the user and print the square of the number in a message
+
+# input a word and blank out all letters that are not vowels with '='
+
+# Create a list of words - go through the list and sum the total number of characters in all words
+
+# Creata a list of words - count the total number of DIFFERENT characters in all the words
+
+# Sum the values in a 3 x 3 2D list of numbers
+
+# Write a function to return if a letter is a vowel or not
+
+# Start with a list of workds.  Create a dictionary to count how many words are of each length. 
+
+
