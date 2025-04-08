3369bdc18cfef873b77ce269c96f5a449a3c6187
darrencotterill <darren.cotterill2@det.nsw.edu.au>
2025-04-08 00:01:29 +0000

    Add the test helper example

---
+++ sum_function.py
@@ -0,0 +1,13 @@
+from testhelper import test
+
+
+def sum(a, b):
+    return a + b
+
+
+
+
+# You can ignore this test code other than to look at the example outputs and inputs
+test("Sum 1", 7, sum(1, 6))
+test("Sum 2", 20, sum(2, 18))
+test("Sum 3", 5, sum(1, 6))
